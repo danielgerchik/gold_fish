258bca8b25a243db038bb4fe4ad8aab3e747a4a7
Daniel <danielgerchik1@gmail.com>
2025-03-14 14:53:46 +0300

    5

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const burger = document.querySelector('.header__burger');
-burger.addEventListener('click', e => {
-    burger.classList.toggle('active');
-    document.querySelector('.header__wrapper').classList.toggle('active');
-});
