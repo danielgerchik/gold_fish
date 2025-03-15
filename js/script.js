5fa76194d5463a8f98bde0a7dd1d64c43c93cfed
Daniel <danielgerchik1@gmail.com>
2025-03-15 23:44:57 +0300

    15

--- js/script.js
+++ js/script.js
@@ -20,12 +20,15 @@ new Swiper(".swiper", {
 });
 // popup
 const popup = document.querySelector(".order-popup");
-const openPopup = document.querySelector(".firstscreen__button");
+const openPopups = document.querySelectorAll(".btn");
 const closePopup = document.querySelector(".order-popup__exit-img");
 
-openPopup.addEventListener("click", () => {
-    popup.classList.add("active");
-});
+    openPopups.forEach(btn => {
+        btn.addEventListener("click", () => {
+            popup.classList.add("active");
+        })  
+    })
+
 
 closePopup.addEventListener("click", () => {
     popup.classList.remove("active");
